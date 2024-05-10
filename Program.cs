122968a5cc7a8f4032e4b6f5a893edbfc14b4e59
Martin <martin_lenart@icloud.com>
2024-05-10 12:16:01 +0200

    update

--- Program.cs
+++ Program.cs
@@ -10,14 +10,19 @@ class Program
     #region Replaced by new model methods
     private static void WriteModel(List<csFriend> _modelList)
     {
+        /*
         foreach (var friend in _modelList)
         {
             Console.WriteLine(friend);
         }
+        */
 
         Console.WriteLine($"NrOfFriends: {_modelList.Count()}");
         Console.WriteLine($"NrOfFriends without any pets: {_modelList.Count(f => f.Pets == null)}");
         Console.WriteLine($"NrOfFriends without an adress: {_modelList.Count(f => f.Adress == null)}");
+        
+        Console.WriteLine($"First Friend: {_modelList.First()}");
+        Console.WriteLine($"Last Friend: {_modelList.Last()}");
     }
 
     private static List<csFriend> SeedModel()
