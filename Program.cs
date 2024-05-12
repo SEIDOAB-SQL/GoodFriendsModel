e07ca88db6323e7f97bceb45c3318f43212a10e3
Martin <martin_lenart@icloud.com>
2024-05-12 20:08:27 +0200

    update

--- Program.cs
+++ Program.cs
@@ -12,16 +12,11 @@ class Program
     #region Replaced by new model methods
     private static void WriteModel(List<csFriend> _modelList)
     {
-        /*
-        foreach (var friend in _modelList)
-        {
-            Console.WriteLine(friend);
-        }
-        */
-
         Console.WriteLine($"NrOfFriends: {_modelList.Count()}");
-        Console.WriteLine($"NrOfFriends without any pets: {_modelList.Count(f => f.Pets == null)}");
-        Console.WriteLine($"NrOfFriends without an adress: {_modelList.Count(f => f.Adress == null)}");
+        Console.WriteLine($"NrOfFriends without any pets: {_modelList.Count(
+            f => f.Pets == null || f.Pets?.Count == 0)}");
+        Console.WriteLine($"NrOfFriends without an adress: {_modelList.Count(
+            f => f.Adress == null)}");
         
         Console.WriteLine($"First Friend: {_modelList.First()}");
         Console.WriteLine($"Last Friend: {_modelList.Last()}");
@@ -42,12 +37,11 @@ class Program
             _goodfriends[i].Adress = (_seeder.Bool) ? _seeder.FromList(_adresses) :null;
 
             //Create between 0 and 3 pets
-            var _pets = new List<csPet>();
+            _goodfriends[i].Pets = new List<csPet>();
             for (int c = 0; c < _seeder.Next(0,4); c++)
             {
-                _pets.Add(new csPet().Seed(_seeder)); 
+                _goodfriends[i].Pets.Add(new csPet().Seed(_seeder)); 
             }
-            _goodfriends[i].Pets = (_pets.Count > 0) ? _pets : null;
         }
         return _goodfriends;
     }
